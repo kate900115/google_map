44b51e577931ec77c222c7a7f20828bff5ce4caa
Ubuntu <ubuntu@ip-172-31-26-141.us-west-2.compute.internal>
2017-06-25 13:27:17 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const app = express()
-const PORT_NUMBER = 8080
+const express = require('express');
+const app = express();
+const path = require('path');
+const GOOGLEAPI_KEY = 'AIzaSyD9SwX4CKr1CIRSVehIMKNsbHi2StHbCkM';
+const PORT_NUMBER = 8080;
+
 app.get('/', function(req, res){
-	res.send('Yuxuan Zhang!')
-})
+	res.sendFile(path.join(__dirname + '/templates/mymap.html'));
+});
 
 app.listen(PORT_NUMBER, function(){
 	console.log('Example app listening on port '+PORT_NUMBER+'!')
-})
+});
